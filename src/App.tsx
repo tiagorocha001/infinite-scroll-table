8c4bf24dbb6e8b6f248ccf9e9f7abcbfdd536938
tiagorocha001 <ttavares@cpat.com>
2025-02-18 05:48:11 -0300

    Fix width

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const PostsPage: React.FC = () => {
   };
 
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth={false}>
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Posts Table
